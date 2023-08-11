2f572810290d9b0c4156da5bdad9e66f19d7cbaa
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-08-11 17:28:05 +0100

    connected mailchimp api

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 - How to render static stylesheets(css) and images
   app.use(express.static("public"))
 - Bootstrap html configuration
+- Mailchimp API
+- Posting data to a server via the server's API
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -11,9 +13,58 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  res.send("loaded");
+  const firstName = req.body.firstname;
+  const secondName = req.body.secondname;
+  const email = req.body.email;
+  console.log(firstName, secondName, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: secondName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/09a06ba5c8";
+
+  const options = {
+    method: "POST",
+    auth: "nwekemaxwell:9ce7e3e386c91f4b710af4588b060faa-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
 });
+
+//API KEY
+// 9ce7e3e386c91f4b710af4588b060faa-us21
+// List ID
+//  09a06ba5c8
